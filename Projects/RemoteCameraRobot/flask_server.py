4daec85191a4a95b8dcda1448de82554146e1fb7
Oyewale Oyediran <waleoyediran@gmail.com>
2017-11-10 11:41:03 +0100

    added Ps4 controller functionality

--- Projects/RemoteCameraRobot/flask_server.py
+++ Projects/RemoteCameraRobot/flask_server.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, render_template, request, Response, send_from_directory, url_for
-from easygopigo3 import EasyGoPiGo3
+# from easygopigo3 import EasyGoPiGo3
 
 MAX_FORCE = 5.0
 MIN_SPEED = 100
 MAX_SPEED = 300
-gopigo3_robot = EasyGoPiGo3()
+# gopigo3_robot = EasyGoPiGo3()
 
 app = Flask(__name__, static_url_path='')
 
@@ -24,33 +24,35 @@ def robot_commands():
     if state == 'move':
         # for moving backward
         if angle_degrees >= 260 and angle_degrees <= 280:
-            gopigo3_robot.set_speed(determined_speed)
-            gopigo3_robot.backward()
+            # gopigo3_robot.set_speed(determined_speed)
+            # gopigo3_robot.backward()
+            pass
 
         # for moving to the left or forward
         if angle_degrees > 90 and angle_degrees < 260:
-            gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_RIGHT, determined_speed)
+            # gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_RIGHT, determined_speed)
 
             left_motor_percentage = abs((angle_degrees - 170) / 90)
             sign = -1 if angle_degrees >= 180 else 1
 
-            gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_LEFT, determined_speed * left_motor_percentage * sign)
+            # gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_LEFT, determined_speed * left_motor_percentage * sign)
 
         # for moving to the right (or forward)- upper half
         if angle_degrees < 90 and angle_degrees >= 0:
-            gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_LEFT, determined_speed)
+            # gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_LEFT, determined_speed)
 
             right_motor_percentage = angle_degrees / 90
-            gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_RIGHT, determined_speed * right_motor_percentage)
+            # gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_RIGHT, determined_speed * right_motor_percentage)
         # for moving to the right (or forward)- bottom half
         if angle_degrees <= 360 and angle_degrees > 280:
-            gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_LEFT, determined_speed)
+            # gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_LEFT, determined_speed)
 
             right_motor_percentage = (angle_degrees - 280) / 80 - 1
-            gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_RIGHT, determined_speed * right_motor_percentage)
+            # gopigo3_robot.set_motor_dps(gopigo3_robot.MOTOR_RIGHT, determined_speed * right_motor_percentage)
 
     elif state == 'stop':
-        gopigo3_robot.stop()
+        # gopigo3_robot.stop()
+        pass
     else:
         app.logger.warning('unknown state sent')
 
